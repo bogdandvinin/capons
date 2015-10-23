dbc4cb99266b4f60bd3208e9dfcc51f2f8d2ea63
capons <bogdan.dvinin@rambler.ru>
2015-10-23 10:49:07 +0300

    Next commit

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
+ааааааааааааааааааааааааааааааааааааааааааааааа
 </head>
 <body>
 <div class="row">
